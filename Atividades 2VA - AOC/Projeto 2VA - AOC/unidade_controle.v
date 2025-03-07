d17bf94c8b35ec8ceef755978b60e770688c267a
Samuel Roberto <developer.snowstorm590@passinbox.com>
2025-03-07 16:33:56 -0300

    Mais instruções adicionadas

--- Atividades 2VA - AOC/Projeto 2VA - AOC/unidade_controle.v
+++ Atividades 2VA - AOC/Projeto 2VA - AOC/unidade_controle.v
@@ -27,26 +27,81 @@ module unidade_controle(opcode, RegDst, Branch, MemRead, MemToReg, ALUOp, MemWri
 	 
 		case(opcode)
 	 // Instrucao lw
-		6'b100011
+		6'b100011: begin
 			ALUSrc   = 1;
 			MemToReg = 1;
 			RegWrite = 1;
 			ALUOp    = 2'b00;
 		end
 	 // Instrucao sw
-	   6'b101011
+	   6'b101011: begin
 			ALUSrc	= 1;
 			MemWrite = 1;
 			ALUOp		= 2'b00;
 			
 		end
 	 // Instrucoes tipo R
-		6'b000000
+		6'b000000: begin
 			RegDst   = 1;
 			RegWrite = 1;
 			ALUOp    = 2'b10;
 		end
+		// Instrucao BEQ
+		6'b000100: begin
+		Branch=1
+		ALUOp=01
+		end
 		
+		//Instrucao addi
+		6'b001000: begin
+		// Instrução J (Jump)
+		6'b000010: begin
+			Branch = 1;
+		end
+
+            // Instrução JAL (Jump and Link)
+		6'b000011: begin          
+			RegWrite = 1;
+			Branch = 1;
+		end
+
+            // Instrução ADDI (Add Immediate)
+		6'b001000: begin
+			ALUSrc   = 1;
+			RegWrite = 1;
+			ALUOp    = 2'b00;
+		end
+
+            // Instrução ANDI (And Immediate)
+		6'b001100: begin
+			ALUSrc   = 1;
+			RegWrite = 1;
+			ALUOp    = 2'b11;
+		end
+
+            // Instrução ORI (Or Immediate)
+		6'b001101: begin
+			ALUSrc   = 1;
+			RegWrite = 1;
+			ALUOp    = 2'b11;
+		end
+
+            // Instrução SLTI (Set Less Than Immediate)
+				
+		6'b001010: begin
+			ALUSrc   = 1;
+			RegWrite = 1;
+			ALUOp    = 2'b11;			
+		end
+
+		
+		ALUSrc=1
+		RegWrite=1
+		end
+		default: begin
+		end
 	 endcase
+	 
+	end
 	
 endmodule 
